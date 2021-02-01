1b904c0558e585c8de2961ee6a934f2498b67193
Akeem King <akeemtlking@gmail.com>
2021-02-01 12:56:40 -0500

    More code cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ import { UploadButton } from './UploadButton';
 
 import { playAudio } from '../lib/media-player/audio';
 
-
 const darkTheme = createMuiTheme({
     palette: {
         type: 'dark'
@@ -21,7 +20,7 @@ const FileExt = {
 const App = props => {
     const [track, setTrack] = useState(null);
     const [folder, setFolder] = useState(props.rootFolder);
-    const [paths, setPaths] = useState([]);
+    const [selectedFilePaths, setSelectedFilePaths] = useState([]);
 
     // Determines if song is currently being played
     const isPlaying = song => track && track.track === song;
@@ -41,9 +40,10 @@ const App = props => {
     const handlePlay = () => {
         // If track is already playing then stop it and start new track.
         if (track && track.playing) stopAudio();
-        playAudio(paths[0]).then(audio => {
+        const currentTrack = selectedFilePaths[0];
+        playAudio(currentTrack).then(audio => {
             setTrack({
-                track: paths[0],
+                track: currentTrack,
                 audio,
                 playing: true
             });
@@ -53,36 +53,61 @@ const App = props => {
     const handleStop = () => stopAudio();
 
     const handleDelete = () => {
-        setFolder(folder.remove(paths));
-        paths.forEach(currentPath => {
-            if (isPlaying(currentPath)) stopAudio();
+        setFolder(folder.remove(selectedFilePaths));
+        selectedFilePaths.forEach(filePath => {
+            if (isPlaying(filePath)) stopAudio();
         });
     };
 
-    const handleNewFiles = newFiles => {
-        setFolder(folder.add(newFiles));
+    const handleNewFiles = newFiles => setFolder(folder.add(newFiles));
+
+
+    const renderPlay = () => {
+        // Only a single file can be played at a time and the play button isn't displayed if the file is being played.
+        const displayPlayButton = selectedFilePaths.length === 1 && (!track || !track.playing);
+        if (displayPlayButton) {
+            return (
+                <IconButton onClick={handlePlay}>
+                    <PlayCircleFilled/>
+                </IconButton>
+            );
+        }
     };
+
+    const renderStop = () => {
+        // Stop is displayed if a track is playing
+        const displayStopButton = track && track.playing;
+        if (displayStopButton) {
+            return (
+                <IconButton onClick={handleStop}>
+                    <Stop/>
+                </IconButton>
+            );
+        }
+    }
+
+    const renderDelete = () => {
+        const displayDeleteButton = selectedFilePaths.length > 0; 
+        if (displayDeleteButton) {
+            return (
+                <IconButton onClick={handleDelete}>
+                    <Delete/>
+                </IconButton> 
+            );
+        }
+    }
 
     return (
             <div className='app'>
                 <ThemeProvider theme={darkTheme}>
                     <Paper>
                     <UploadButton onNewFiles={handleNewFiles}/>
-                    {paths.length === 1 && (!track  || !track.playing) ? 
-                        <IconButton onClick={handlePlay}>
-                            <PlayCircleFilled/>
-                        </IconButton> : null}
-                    {track && track.playing ?
-                        <IconButton onClick={handleStop}>
-                            <Stop/>
-                        </IconButton> : null}
-                    {paths.length > 0 ?
-                        <IconButton onClick={handleDelete}>
-                            <Delete/>
-                        </IconButton> : null}
-                        <FileTree root={folder} fileExtFilter={FileExt.MP3} onNodeSelect={filePaths => {
-                            setPaths(filePaths);
-                        }}/>
+                    {renderPlay()}
+                    {renderStop()}
+                    {renderDelete()}
+                    <FileTree root={folder} fileExtFilter={FileExt.MP3} onNodeSelect={filePaths => {
+                        setSelectedFilePaths(filePaths);
+                    }}/>
                     </Paper>
                 </ThemeProvider>
             </div>
